f10c23d900da7edb66b241649c5a1f3d25f9992c
Alessandra Germano <aagermanot@gmail.com>
2022-03-24 22:24:53 +0000

    changes made from theia lab environment

--- script.js
+++ script.js
@@ -1,6 +1,33 @@
+// Calculates the final value 
 function compute()
 {
-    p = document.getElementById("principal").value;
+    // p = document.getElementById("principal").value;
+
+    var principal = document.getElementById("principal").value;
+
+    if (document.getElementById("principal").value <= 0) {
+        alert ("Enter a positive number");
+        document.getElementById("principal").focus();
+        return false;
+    }
+     
+    var rate = document.getElementById("rate").value;
+    var years = document.getElementById("years").value;
+    var interest = principal * years * rate /100;
+    var year = new Date().getFullYear()+parseInt(years);
+
+    // creates the string with the result evaluation 
+    document.getElementById("result").innerHTML = 
+    "<br><br/>" + "If you deposit <mark>" + principal + "</mark>," +
+    "<br/>" + "at an interest rate of <mark>" + rate + "</mark>%." +
+    "<br/>" + "You will receive an amount of <mark>" +  interest + "</mark>," +
+    "<br/>" + "in the year <mark>" + year + "</mark>";
     
 }
-        
+// Updates value on the slide element
+function updateRate()
+{
+    var rateval = document.getElementById("rate").value;
+    document.getElementById("rate_val").innerText=rateval;
+    
+}
